6f139de768bfaf354048da30873a0a183a5d0e39
hyun\Hyun Jeong <LIMH0141@e.ntu.edu.sg>
2017-09-04 23:54:32 +0800

    initialize map added

--- src/mdp/Map.java
+++ src/mdp/Map.java
@@ -5,6 +5,16 @@ import mdp.Constants;
 import java.util.ArrayList;
 
 public class Map {
-	private final ArrayList<Coordinates> coordinates = new ArrayList<Coordinates>();
+	private final ArrayList<Coordinates> fullMap = new ArrayList<Coordinates>();
 	
+	public Map(){
+		
+		//initialize map
+		for(int i=1; i<= Constants.MAX_X; i++){
+			for(int j=1; j<=Constants.MAX_Y; j++ ){
+			mdp.Coordinates coordinates = new Coordinates(i,j);
+			fullMap.add(coordinates);
+			}
+		}
+	}
 }
