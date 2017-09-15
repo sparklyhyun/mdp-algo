b328a2b36f07e0447f662dc706b9bb2df1927bc8
SanjushaGuvvala <31960718+SanjushaGuvvala@users.noreply.github.com>
2017-09-15 14:15:25 +0800

    Update Simulator.java

--- src/mdp/Simulator.java
+++ src/mdp/Simulator.java
@@ -12,8 +12,8 @@ import java.awt.event.MouseEvent;
 public class Simulator {
 	private static JFrame _mapFrame = null;          // JFrame to display Map
 
-	private static JPanel _mapView = null;           // JPanel for map views
-	private static JPanel _mapbuttons = null;        // JPanel for buttons
+	private static JPanel _mapBox = null;           // JPanel for map Box
+	private static JPanel _mapButtons = null;        // JPanel for map buttons
 
 	private static Robot robot;
 
@@ -22,26 +22,28 @@ public class Simulator {
 
 	private static int timeLimit = 3600;            // time limit
 	private static int coverageLimit = 300;         // coverage limit
-
+	
+	private static final CommMgr comm = CommMgr.getCommMgr();
 
 	private static final boolean realExecution = true;
 
 
 	public static void main(String[] args) {
-	if (realExecution) comm.openConnection();   //connection function to be added!!!
+		if (realExecution) comm.openConnection();   //connection function to be added!!!
 
-	robot = new Robot(Constants.START_X, Constants.START_Y, realExecution);
+		robot = new Robot(Constants.START_X, Constants.START_Y, realExecution);
 
-	if (!realExecution) {
-	    realMap = new Map(robot);
-	    realMap.setAllUnexplored();
-	}
+		if (!realExecution) {
+		    realMap = new Map(robot);
+		    realMap.setAllUnexplored();
+		}
 
-	exploredMap = new Map(robot);
-	exploredMap.setAllUnexplored();
+		exploredMap = new Map(robot);
+		exploredMap.setAllUnexplored();
 
-	viewFullMap();
+		viewFullMap();
 	}	
+	
 	
 	private static void viewFullMap() {
 		_mapFrame = new JFrame();
@@ -50,7 +52,87 @@ public class Simulator {
 		_mapFrame.setResizable(false);
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
 		_mapFrame.setLocation(dimension.width / 2 - _mapFrame.getSize().width / 2, dimension.height / 2 - _mapFrame.getSize().height / 2);
-		
+        
+		// Create the BoxLayout for storing the different maps
+		_mapBox = new JPanel(new BoxLayout());
+
+		// Create the JPanel for the buttons
+		_mapButtons = new JPanel();
+
+		// Add _mapBox & _mapButtons to the main frame's content pane
+		Container contentPane = _mapFrame.getContentPane();
+		contentPane.add(_mapBox, BorderLayout.CENTER);
+		contentPane.add(_mapButtons, BorderLayout.PAGE_END);
+
+		// Initialize the main map layout
+		initMainLayout();
+
+		// Initialize the map buttons
+		initButtonsLayout();
+
+		// View full display of the application
+		_mapFrame.setVisible(true);
+		_mapFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        }
+	
+	private static void initMainLayout() {
+		if (!realExecution) {
+		    _mapBox.add(realMap, "REAL_MAP");
+		}
+		_mapBox.add(exploredMap, "EXPLORATION");
+
+		BoxLayout b = ((BoxLayout) _mapBox.getLayout());
+		if (!realExecution) {
+		    blshow(_mapBox, "REAL_MAP");
+		} else {
+		    blshow(_mapBox, "EXPLORATION");
+		}
+	}
+	
+	private static void initButtonsLayout() {
+		_buttons.setLayout(new GridLayout());
+		addLoadMapButton();
+	}
+	
+	private static void formatButton(JButton btn) {
+		btn.setFont(new Font("Calibri", Font.BOLD, 12));
+		btn.setFocusPainted(false);
+	}
+	
+	private static void addLoadMapButton() {
+		if (!realExecution){
+		    // Load Map Button
+		    JButton LoadMap_btn = new JButton("Load Map");
+		    formatButton(LoadMap_btn);
+		    LoadMap_btn.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+			    JDialog loadMapDialog = new JDialog(_mapFrame, "Load Map", true);
+			    loadMapDialog.setSize(350, 50);
+			    loadMapDialog.setLayout(new FlowLayout());
+
+			    final JTextField loadText = new JTextField(13);
+			    JButton loadMapButton = new JButton("Load");
+
+			    loadMapButton.addMouseListener(new MouseAdapter() {
+				public void mouseClicked(MouseEvent e) {
+				    loadMapDialog.setVisible(false);
+				    loadMapFromDisk(realMap, loadText.getText());
+				    BoxLayout bl = ((BoxLayout) _mapBox.getLayout());
+				    bl.show(_mapBox, "REAL_MAP");
+				    realMap.repaint();
+				}
+			    });
+
+			    loadMapDialog.add(new JLabel("File Name: "));
+			    loadMapDialog.add(loadText);
+			    loadMapDialog.add(loadMapButton);
+			    loadMapDialog.setVisible(true);
+			}
+		    });
+		    _buttons.add(LoadMap_btn);
+	}
+	
+
 	
 
 }
